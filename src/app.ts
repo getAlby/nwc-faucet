c12e0fbf430ac3179d172fa3caf48397f65427a4
Roland Bewick <roland.bewick@gmail.com>
2026-05-13 20:23:07 +0700

    chore: adjust page size

--- src/app.ts
+++ src/app.ts
@@ -109,7 +109,7 @@ async function deleteApp(appPubkey: string): Promise<void> {
 }
 
 async function cleanupUnusedApps(): Promise<void> {
-  const pageSize = 5;
+  const pageSize = 20;
   let offset = 0;
   let pageLength = pageSize;
   let numScanned = 0;
